1133cbfe08b5e0300acf17e3e93691f738ddaccd
acktarius <xchaps@aol.com>
2024-10-10 00:17:14 -0400

    readMiningConfig()

--- xmrstak/setup_gui.cpp
+++ xmrstak/setup_gui.cpp
@@ -7,10 +7,59 @@
 #include <wx/filename.h>
 #include <wx/msgdlg.h>
 #include <wx/stdpaths.h>
+#include <wx/textfile.h>
+#include <wx/string.h>
 
 enum {
     ID_START_BUTTON = wxID_HIGHEST + 1
 };
+//readMiningConfig
+std::tuple<bool, std::string, std::string> readMiningConfig()
+{
+    wxString poolsFilePath = wxFileName::GetCwd() + wxFileName::GetPathSeparator() + "pools.txt";
+    bool poolsFileExists = wxFileExists(poolsFilePath);
+
+    if (!poolsFileExists)
+    {
+        return std::make_tuple(false, std::string(), std::string());
+    }
+
+    wxTextFile file(poolsFilePath);
+    if (!file.Open())
+    {
+        return std::make_tuple(false, std::string(), std::string());
+    }
+
+    wxString poolAddress, walletAddress;
+    for (wxString line = file.GetFirstLine(); !file.Eof(); line = file.GetNextLine())
+    {
+        if (line.StartsWith("\"pool_address\" : "))
+        {
+            poolAddress = line.AfterFirst(':').Trim(true).Trim(false);
+            poolAddress.Remove(0, 1).RemoveLast();
+        }
+        else if (line.StartsWith("\"wallet_address\" : "))
+        {
+            walletAddress = line.AfterFirst(':').Trim(true).Trim(false);
+            walletAddress.Remove(0, 1).RemoveLast();
+        }
+
+        if (!poolAddress.IsEmpty() && !walletAddress.IsEmpty())
+        {
+            break;
+        }
+    }
+
+    file.Close();
+
+    if (poolAddress.IsEmpty() || walletAddress.IsEmpty())
+    {
+        return std::make_tuple(true, std::string(), std::string());  //TODO: change to false when pools.txt is fixed
+    }
+
+    return std::make_tuple(true, poolAddress.ToStdString(), walletAddress.ToStdString());
+}
+
 // Implement the member functions of MiningConfigFrame
 MiningConfigFrame::MiningConfigFrame(wxWindow* parent, wxWindowID id, const wxString& title, 
                                      const wxPoint& pos, const wxSize& size)
@@ -25,28 +74,29 @@ void MiningConfigFrame::OnStart(wxCommandEvent& event)
     // Your OnStart implementation here
 }
 
+
 // Implement the member functions of MyApp
 bool GUIApp::OnInit()
+{
+    if (!wxApp::OnInit())
+        return false;
+
+    // Create an instance of ReadPoolConfig
+    auto [fileReachable, pool, wallet] = readMiningConfig();
+    
+    if (fileReachable == false)
     {
-        if (!wxApp::OnInit())
-            return false;
-
-        // Check for pool.txt file
-        wxString poolFilePath = wxFileName::GetCwd() + wxFileName::GetPathSeparator() + "pools.txt";
-
-        if (!wxFileExists(poolFilePath))
-        {
-            wxMessageBox("The pools.txt file is missing in the /bin folder. "
-                         "Please create this file running xmr-stak_initial setup before starting the miner this way.",
-                         "Configuration Missing",
-                         wxOK | wxICON_ERROR);
-            return false;
-        }
-
-        MiningConfigFrame* frame = new MiningConfigFrame(nullptr, wxID_ANY, "XMR-Stak Day2Day Mining", wxDefaultPosition, wxSize(400, 300));
-        frame->Show(true);
-        return true;
+        wxMessageBox("The pools.txt file is missing or invalid in the /bin folder. "
+                     "Please create this file by running xmr-stak_initial setup before starting the miner this way.",
+                     "Configuration Missing",
+                     wxOK | wxICON_ERROR);
+        return false;
     }
 
+    MiningConfigFrame* frame = new MiningConfigFrame(nullptr, wxID_ANY, "XMR-Stak Day2Day Mining", wxDefaultPosition, wxSize(400, 300));
+    frame->SetPoolInfo(pool, wallet);  // Assuming you have this method in MiningConfigFrame
+    frame->Show(true);
+    return true;
+}
 
 wxIMPLEMENT_APP(GUIApp);
--- xmrstak/setup_gui.hpp
+++ xmrstak/setup_gui.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <tuple>
+#include <string>
 
 class MiningConfigFrame : public wxFrame
 {
@@ -9,15 +11,37 @@ public:
                     const wxPoint& pos = wxDefaultPosition, 
                     const wxSize& size = wxDefaultSize);
 
+    void SetPoolInfo(const std::string& pool, const std::string& wallet) {
+        m_pool = pool;
+        m_wallet = wallet;
+        // Update GUI elements if necessary
+    }
+
 private:
-
     void OnStart(wxCommandEvent& event);
-
+    std::string m_pool;
+    std::string m_wallet;
     // Other private members and methods
+};
+
+class PoolConfig
+{
+public:
+    PoolConfig(bool fileReachable, const std::string& poolAddress, const std::string& walletAddress);
+
+    std::tuple<bool, std::string, std::string> read();
+
+private:
+    bool m_fileReachable;
+    std::string m_poolAddress;
+    std::string m_walletAddress;
 };
 
 class GUIApp : public wxApp
 {
 public:
     virtual bool OnInit() override;
+
+private:
+    // You might want to add any private members here if needed
 };
