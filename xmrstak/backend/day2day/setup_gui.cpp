792bee50140c71650e1e6eabc015eccd8d494f80
acktarius <xchaps@aol.com>
2024-11-23 17:57:29 -0500

    progress

--- xmrstak/backend/day2day/setup_gui.cpp
+++ xmrstak/backend/day2day/setup_gui.cpp
@@ -10,8 +10,8 @@
 #include <wx/textfile.h>
 #include <wx/string.h>
 #include <fstream>
-#include "../../jconf.hpp"
-#include "../../params.hpp"
+#include <jsoncpp/json/json.h>
+#include <sstream>
 
 
 enum {
@@ -20,14 +20,74 @@ enum {
 
 std::tuple<bool, std::string, std::string, std::string> readMiningConfig()
 {
-    std::cout << "DEBUG: Starting readMiningConfig()" << std::endl;
-       
-    // Get pool address and wallet directly from jconf instance
-    const std::string& poolAddr = jconf::GetDefaultPool(xmrstak::params::inst().poolURL.c_str());
-    const std::string& walletAddr = jconf::GetDefaultPool(xmrstak::params::inst().poolUsername.c_str());
-    const std::string& currency = jconf::GetDefaultPool(xmrstak::params::inst().currency.c_str());
+    try {
+        std::ifstream file("pools.txt", std::ifstream::binary);
+        if (!file.is_open()) {
+            return std::make_tuple(false, "", "", "");
+        }
 
-    return std::make_tuple(!poolAddr.empty(), poolAddr, walletAddr, currency);
+        // Read file content into string
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string content = buffer.str();
+
+        // Remove C-style comments
+        size_t start, end;
+        while ((start = content.find("/*")) != std::string::npos) {
+            if ((end = content.find("*/", start)) != std::string::npos) {
+                content.erase(start, end - start + 2);
+            }
+        }
+
+        // Remove single-line comments
+        while ((start = content.find("//")) != std::string::npos) {
+            if ((end = content.find('\n', start)) != std::string::npos) {
+                content.erase(start, end - start);
+            } else {
+                content.erase(start);
+            }
+        }
+
+        // Add enclosing braces if not present
+        if (content.find_first_not_of(" \t\n\r") != '{') {
+            content = "{" + content + "}";
+        }
+
+        // Parse JSON
+        Json::Value root;
+        Json::CharReaderBuilder builder;
+        builder["collectComments"] = false;
+        std::string errs;
+        std::istringstream jsonStream(content);
+
+        if (!Json::parseFromStream(builder, jsonStream, &root, &errs)) {
+            return std::make_tuple(false, "", "", "");
+        }
+
+        if (!root.isMember("pool_list") || !root["pool_list"].isArray()) {
+            return std::make_tuple(false, "", "", "");
+        }
+
+        const Json::Value& poolList = root["pool_list"];
+        if (poolList.empty()) {
+            return std::make_tuple(false, "", "", "");
+        }
+
+        std::string poolAddress = poolList[0]["pool_address"].asString();
+        std::string walletAddress = poolList[0]["wallet_address"].asString();
+        std::string currency = root["currency"].asString();
+
+        return std::make_tuple(true, poolAddress, walletAddress, currency);
+    }
+    catch (const Json::Exception& e) {
+        return std::make_tuple(false, "", "", "");
+    }
+    catch (const std::exception& e) {
+        return std::make_tuple(false, "", "", "");
+    }
+    catch (...) {
+        return std::make_tuple(false, "", "", "");
+    }
 }
 
 // Implement the member functions of MiningConfigFrame
@@ -48,17 +108,11 @@ void MiningConfigFrame::OnStart(wxCommandEvent& event)
 // Implement the member functions of MyApp
 bool GUIApp::OnInit()
 {
-    std::cout << "DEBUG: Starting OnInit()" << std::endl;
-    
     if (!wxApp::OnInit())
         return false;
 
-    std::cout << "DEBUG: About to call readMiningConfig()" << std::endl;
-    
     // Create an instance of ReadPoolConfig
     auto [fileReachable, pool, wallet, currency] = readMiningConfig();
-    
-    std::cout << "DEBUG: readMiningConfig() returned fileReachable=" << fileReachable << std::endl;
     
     if (fileReachable == false)
     {
@@ -68,13 +122,22 @@ bool GUIApp::OnInit()
                      wxOK | wxICON_ERROR);
         return false;
     }
-     {
-        wxMessageBox("The pools.txt file has been detect,"
-                     + pool + " " + wallet + " " + currency,
-                     "Configuration",
-                     wxOK | wxICON_INFORMATION);
-    }
-    MiningConfigFrame* frame = new MiningConfigFrame(nullptr, wxID_ANY, "XMR-Stak Day2Day Mining", wxDefaultPosition, wxSize(400, 300));
+      if (currency != "conceal")
+        {
+        wxMessageBox("this user interface is developped for Conceal users.\n",
+                     "Please consider mining CCX",
+                     wxOK | wxICON_ERROR);
+        return false;
+        }
+        {
+            wxMessageBox("The pools.txt file has been detected\n"
+                        "Pool: " + wxString(pool) + "\n"
+                        "Wallet: " + wxString(wallet) + "\n"
+                        "Mining: " + wxString(currency),
+                        "Configuration found",
+                        wxOK | wxICON_INFORMATION);
+        }
+    MiningConfigFrame* frame = new MiningConfigFrame(nullptr, wxID_ANY, "XMR-Stak Day2Day Mining", wxDefaultPosition, wxSize(600, 400));
     frame->SetPoolInfo(pool, wallet);  // Assuming you have this method in MiningConfigFrame
     frame->Show(true);
     return true;
