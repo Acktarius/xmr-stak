d213bad6d316eca384cda0e2e5b4c19003126e02
acktarius <xchaps@aol.com>
2024-11-25 00:28:37 -0500

    change initial pool input
    * press enter to validate default pool

--- xmrstak/cli/cli-miner.cpp
+++ xmrstak/cli/cli-miner.cpp
@@ -57,6 +57,8 @@
 
 int do_benchmark(int block_version, int wait_sec, int work_sec);
 
+std::string get_pool_input(const char* prompt, const char* default_value);
+
 void help()
 {
 	using namespace std;
@@ -242,17 +244,24 @@ void do_guided_pool_config()
 	if(pool.empty())
 	{
 		prompt_once(prompted);
-
 		userSetPool = false;
-		std::cout << "- Pool address: e.g. " << jconf::GetDefaultPool(currency.c_str()) << std::endl;
-		std::cin >> pool;
+		
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		
+		std::cout << "- Enter pool address and port, e.g. url:port" << std::endl;
+		pool = get_pool_input("or press enter for default pool:",
+                     jconf::GetDefaultPool(currency.c_str()));
+                     
+		std::cout << std::endl;
 	}
 
 	auto userName = params::inst().poolUsername;
 	if(userName.empty())
 	{
 		prompt_once(prompted);
-
+		
+		std::cout << std::endl;
 		std::cout << "- Username (wallet address or pool login):" << std::endl;
 		std::cin >> userName;
 	}
@@ -352,6 +361,19 @@ void do_guided_pool_config()
 	configTpl.replace("POOLCONF", pool_table);
 	configTpl.write(params::inst().configFilePools);
 	std::cout << "Pool configuration stored in file '" << params::inst().configFilePools << "'" << std::endl;
+}
+
+std::string get_pool_input(const char* prompt, const char* default_value) {
+    std::string tmp;
+    std::cout << prompt << " [" << default_value << "]: " << std::flush;
+    std::getline(std::cin, tmp);
+    
+    std::cout << "Received input: '" << tmp << "'" << std::endl;
+    
+    if(tmp.empty()) {
+        return std::string(default_value);
+    }
+    return tmp;
 }
 
 void do_guided_config()
